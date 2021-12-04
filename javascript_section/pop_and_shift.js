012de8cf29270c560de32b040b430dd80e7073d3
David Melsheimer <dave.melshman@gmail.com>
2021-12-04 14:27:41 -0500

    squeezing some class in

---
+++ javascript_section/pop_and_shift.js
@@ -0,0 +1,13 @@
+const planets = ['The Moon','Venus', 'Earth', 'Mars', 'Jupiter']; //DO NOT TOUCH THIS LINE!
+
+// YOUR CODE GOES BELOW THIS LINE:
+// planets.shift();
+// planets.unshift('Mercury');
+// planets.push('Saturn');
+// console.log(planets);
+
+//splice
+
+let days = ['monday', 'Tuesday', 'Wednesday'];
+days.splice(2, 0, 'Pink', 'Blue');  //inserting multiple items in locatin at index of 2
+console.log(days);

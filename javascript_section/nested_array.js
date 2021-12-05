83de92fdda6ddf8bd8d0170072b93dadc3153c85
David Melsheimer <dave.melshman@gmail.com>
2021-12-05 13:04:05 -0500

    sunday coding with 2022 bootcamp

--- javascript_section/nested_array.js
+++ javascript_section/nested_array.js
@@ -21,8 +21,8 @@ const restaurant = {
     name: 'Ichiran Ramen',
     address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
     city: 'Brooklyn',
-    state: 'NY',
     zipcode: '11206',
+    state: 'NY',
 }
 
 //YOUR CODE GOES DOWN HERE:
@@ -34,3 +34,79 @@ const comments = [
     {username: 'FishBoi', text: 'Glub Glub', votes: 939721}
 ];
 console.log(comments[1].text);
+
+//for loops
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+let lyric = 'Da ba dee da ba daa';
+for(let i = 0; i < 6; i++){
+    console.log(lyric);
+}
+
+//decrementing with for loop:
+// Write a loop that prints:
+// 25
+// 20
+// 15
+// 10
+// 5
+// 0
+for(let i = 25; i >=0; i -= 5){
+    console.log(i);
+}
+
+//ietrating over arrays:
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for(let i = 0; i < people.length; i ++){
+    console.log(people[i].toUpperCase());
+}
+
+const reTest = () => {
+    while( Math.random()*10 > 2){
+        console.log("still Running");
+    }
+};
+
+const secretLoop = () =>{
+    const SECRET = "BabyHippo";
+    let guess = alert('enter secret code!');
+    while(guess !== SECRET){
+        guess = prompt('enter secret code!');
+    }
+};
+
+//fun implementation of break keyword:
+const funnyFunc = ()=>{
+    let input = prompt("Hey you, say something");
+    while(true){
+        input = prompt(input);
+        if(input === "stop copying me"){
+            break;
+        }
+    }
+};
+
+const guessingGame = (maxNum) =>{
+    let target = Math.floor((Math.random()*maxNum)) + 1;
+    let guess = prompt("Take a guess, part-nor");
+    while(parseInt(guess) !== target){
+        if (guess == 'quit'){break;}
+        if(guess > target){
+            guess = prompt("Nope, you guess too high.  Guess again.");
+        }
+        else{
+            guess = prompt("Nope, you guessed too low.  Guess again.")
+        }
+    }
+
+    let finalThoughts = prompt('You got it!!  How do you feel?');
+
+};
+
+//for of loop
+const subReddits = ['cringe', 'books', 'chickens'];
+for(let i of subReddits){
+    console.log(`visit www.reddit.com/subreddit/${i}`);
+}
+

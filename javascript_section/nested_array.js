bb0336929d5f8ed3b5cbfa42ac09032f5424d513
David Melsheimer <dave.melshman@gmail.com>
2021-12-04 22:36:36 -0500

    nested arrays and objects

--- javascript_section/nested_array.js
+++ javascript_section/nested_array.js
@@ -11,3 +11,26 @@ const airplaneSeats = [
 
 airplaneSeats[3][1] = 'Hugo';
 console.log(airplaneSeats);
+
+//Object literal in-browser exercise:
+const product = {name: 'Gummy Bears', inStock: true, price: 1.99, flavors: ['grape', 'apple', 'cherry']};
+
+//Object access exercise
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+const fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`;
+
+//following along with nested arrays and objects:
+const comments = [
+    {username: 'Tammy', text: 'lololol', votes: 9},
+    {username: 'FishBoi', text: 'Glub Glub', votes: 939721}
+];
+console.log(comments[1].text);

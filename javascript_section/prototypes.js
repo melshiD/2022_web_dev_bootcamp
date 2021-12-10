5980c5eab399925755f3033666cdde7434194f20
David Melsheimer <dave.melshman@gmail.com>
2021-12-10 15:20:29 -0500

    more prototypes and design patterns for objects and methods

--- javascript_section/prototypes.js
+++ javascript_section/prototypes.js
@@ -3,31 +3,37 @@
 // }
 
 // 'hello'.yell();
+// function makeColor(r, g, b){
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
+//     color.rgb = function(){
+//         const{r, g, b} = this;
+//         return `rgb(${r}, ${g}, ${b})`;
+//     }
+//     color.hex = function(){
+//         const{r, g, b} = this;
+//         return '#' + ((1<<24)+(r<<16)+(g<<8)+b).toString(16).slice(1);
+//     }
+//     return color;
+// }
 
-function hex(r, g, b){
-    return '#' + ((1<<24)+(r<<16)+(b<<8)+b).toString(16).slice(1);
+// const firstColor = makeColor(34, 234, 3);
+// console.log(firstColor.rgb());
+// console.log(firstColor.hex());
+//--------------------------------------------------
+function Color(r, g, b){     //caps at beginning of func name is stylistic of construct to be used with new keyword
+    this.r = r;
+    this.g = g;
+    this.b = b;
 }
 
-function rgb(r,g,b){
-    return `rgb(${r}, ${g}, ${b})`;
+const newColor = new Color(1, 1, 1);
+
+Color.prototype.hex = function(){
+    const {r, g, b} = this;
+    return '#' + ((1<<24)+(r<<16)+(g<<8)+b).toString(16).slice(1);
 }
 
-function makeColor(r, g, b){
-    const color = {};
-    color.r = r;
-    color.g = g;
-    color.b = b;
-    color.rgb = function(){
-        const{r, g, b} = this;
-        return `rgb(${r}, ${g}, ${b})`;
-    }
-    color.hex = function(){
-        const{r, g, b} = this;
-        return '#' + ((1<<24)+(r<<16)+(g<<8)+b).toString(16).slice(1);
-    }
-    return color;
-}
-
-const firstColor = makeColor(34, 234, 3);
-console.log(firstColor.rgb());
-console.log(firstColor.hex());
+console.log(newColor.hex())

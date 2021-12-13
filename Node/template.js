2dde747af84fa633869016dc66167dbb9fc457a1
David Melsheimer <dave.melshman@gmail.com>
2021-12-12 21:55:25 -0500

    fiddling with file systems commands

--- Node/template.js
+++ Node/template.js
@@ -1,2 +1,7 @@
 const fs = require('fs');
-console.log(fs);
+// fs.mkdir('temp', { recursive: true }, (err) =>{
+//     console.log('in the callback');
+//     if(err) throw err;
+// });
+// console.log('I come after mkdir')
+fs.mkdirSync('cats');

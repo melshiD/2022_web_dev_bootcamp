b0ed5d8f4d1f59cad9c777af27a29c0b77b34631
David Melsheimer <dave.melshman@gmail.com>
2021-12-12 22:13:28 -0500

    leaving off without writeFileSync working.  Check that next

--- Node/template.js
+++ Node/template.js
@@ -4,4 +4,12 @@ const fs = require('fs');
 //     if(err) throw err;
 // });
 // console.log('I come after mkdir')
-fs.mkdirSync('cats');
+
+let folderName = process.argv[2] || 'Project';
+folderName = folderName.toString();
+fs.mkdirSync(folderName);
+
+// fs.writeFileSync(`${folderName}.html, ${folderName}.css, ${folderName}.js`)
+fs.writeFileSync(`${folderName}/test.html`);
+fs.writeFileSync(`${folderName}/test.js`);
+fs.writeFileSync(`${folderName}/test.css`);

7c724d19debf9213a2d721c024a4a68d83aa3bbd
David Melsheimer <dave.melshman@gmail.com>
2021-12-13 16:16:52 -0500

    can NOT get langs to import correctly.  Receiving error that it doenst have an export with the name langs, which it does

---
+++ Node/lang_guesser/index.js
@@ -0,0 +1,7 @@
+import {franc} from 'franc';
+import langs from 'langs';
+// const {langs} = require('langs');
+
+const threeCharCode = franc('Alle menslike wesens word vry');
+
+console.log(threeCharCode);

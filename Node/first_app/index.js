be44dce9539a7febef4ae4df31a8a7a2e1eaf233
David Melsheimer <dave.melshman@gmail.com>
2021-12-20 13:01:53 -0500

    path patterns and responses

--- Node/first_app/index.js
+++ Node/first_app/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-    console.log('We got a new request');
-    res.send('hello, we got your request.  Here is your response')
+// app.use((req, res) => {
+//     console.log('We got a new request');
+//     res.send('<h1>Here is my webpage</h1>')
+// })
+
+app.get('/', (req, res) => {
+    res.send('<h1>Here is my webpage</h1>')
+})
+
+app.get('/r/:subcategory', (req, res) => {
+    const {subcategory} = req.params;
+    res.send(`so you're into ${subcategory}`);
+});
+
+app.get('/r/:subcategory/:postId', (req, res) => {
+    const {subcategory, postId} = req.params;
+    res.send(`so you're into ${subcategory} and ${postId}`);
+});
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!')
+});
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!');
 })
 
 app.listen(3000, () => {

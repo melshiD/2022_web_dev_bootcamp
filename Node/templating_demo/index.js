b8f1931e6aa89bb9554edab428a13452b76671bf
David Melsheimer <dave.melshman@gmail.com>
2022-01-19 11:51:58 -0500

    serving static assets

--- Node/templating_demo/index.js
+++ Node/templating_demo/index.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/r/:subreddit', (req, res) => {
+    const {subreddit} = req.params;
+    const data = redditData[subreddit];
+    if(data){
+        res.render('subreddit', {...data});
+    }
+    else{
+        res.render('notfound', {subreddit});
+    }
+    console.log(data);
+    
+})
+
+app.get('/rand', (req, res) => {
+    let number = Math.floor(Math.random() * 10) + 1;
+    res.render('random', {rand: number});
+});
+
+app.get('/cats', (req, res) => {
+    const cats = [
+        'Blue', 'Rocket', 'Steph', 'Bonker'
+    ];
+    res.render('cats', {cats});
 });
 
 app.listen(3000, () => {

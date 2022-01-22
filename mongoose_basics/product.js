062045c2dddeb8ff664a087917572ce408c59e2e
David Melsheimer <dave.melshman@gmail.com>
2022-01-22 15:13:26 -0500

    schemas are a blueprint for fancy constructors for documents to be saved to collections.  Key-words/trick-phrases = got'em

--- mongoose_basics/product.js
+++ mongoose_basics/product.js
@@ -1,7 +1,7 @@
 const { BulkWriteResult } = require('mongodb');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/shopApp')
-    .then( () => {
+    .then(() => {
         console.log("connection open");
     })
     .catch(err => {
@@ -9,22 +9,68 @@ mongoose.connect('mongodb://localhost:27017/shopApp')
         console.log(err);
     });
 
-    const productSchema = new mongoose.Schema({
-        productName: {type:String,
-                      required: true
-        },
-        price: {type: Number}
+const productSchema = new mongoose.Schema({
+    productName: {
+        type: String,
+        required: true,
+        maxlength: 30,
+        lowercase: true
+    },
 
+    price: {
+        type: Number,
+        min: [0, 'Price needs to be entered as a positive number']
+    },
+
+
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+
+    color: String,
+
+    categories: [String],
+
+    qty: {
+        online: { type: Number, default: 0 },
+        inStore: {type: Number, default: 1}
+    },
+    size: {
+        type: String,
+        enum: {values: ['s', 'm', 'l', 'OSFA'],
+               message: "must be a valid size",
+               default: 'OSFA'}
+    }
+
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+const newSku = new Product({
+    productName: 'Sweet Horn',
+    price: 23.21, color: 'red',
+    categories: ['family', 'outdoor', 'vacation', 232],
+    size: 'OSFA'
+});
+
+
+newSku.save()
+    .then(data => {
+        console.log('product entry saved')
+        // console.log(data);
+    })
+    .catch((err) => {
+        console.log('on no! It didn\'t work');
+        console.log(err);
     });
 
-    const Product = mongoose.model('Product', productSchema);
-
-    const newSku = new Product({productName: 'mountain bike', price: 50111});
-    newSku.save()
-        .then(data => {
-            console.log(data);
-        })
-        .catch( (err) => {
-            console.log('on no! It didn\'t work');
-            console.log(err);
-        });
+Product.findOneAndUpdate({ name: 'Sweet Horn'}, {price: 21.95}, {new: true, runValidators: true})
+    .then( data => {
+        console.log("Product Update Successful");
+        console.log(data);
+    })
+    .catch(err => {
+        console.log('Update Failed:');
+        console.log(err);
+    });

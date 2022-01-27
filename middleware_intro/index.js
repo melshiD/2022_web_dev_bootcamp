5b0dd8a8730769f75450de012e849af46bcc0a8d
David Melsheimer <dave.melshman@gmail.com>
2022-01-27 11:29:47 -0500

    middleware practice

--- middleware_intro/index.js
+++ middleware_intro/index.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const app = express();
+const app = (require('express'))();
+const path = require('path');
 const morgan = require('morgan');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(morgan('common'));
+//adds date and time data to request object
 app.use((req, res, next) => {
-    console.log(req.method.toUpperCase());
+    console.log(req.method.toUpperCase(), req.path);
     const newTime = new Date().getTime();
     const newDate = new Date(newTime);
     req.arrivalTime = newTime.toString();
@@ -12,15 +16,28 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use((req, res, next) => {
+    console.log(req.query);
+    const {password} = req.query;
+    if(password === 'chicken'){
+        next();
+    }
+    res.send('sorry, you need a password');
+});
 
-// app.use((req, res, next) => {
-//     console.log('My first middleware');
-//     next();
-// })
+app.get('/home', (req, res) => {
+    res.render('home');
+});
 
-app.get('/', (req, res) => {
+app.get('/secret', (req, res) => {
     res.send(req.arrivalDate);
 });
+
+app.get('/dogs', (req, res) => {
+    res.send(`your beloved pup will arrive on ${req.arrivalDate}`);
+});
+
+app.use((req, res) => res.send('NOT FOUND'));
 
 app.listen(3000, () => {
     console.log('listening on 3000');

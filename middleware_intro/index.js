67537304c2cf75a3efb0be5dd32750a84c95cc91
David Melsheimer <dave.melshman@gmail.com>
2022-01-27 22:34:10 -0500

    express error handling

--- middleware_intro/index.js
+++ middleware_intro/index.js
@@ -1,6 +1,8 @@
 const app = (require('express'))();
 const path = require('path');
 const morgan = require('morgan');
+const req = require('express/lib/request');
+const { download } = require('express/lib/response');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -16,20 +18,24 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    console.log(req.query);
+const verifyPassword = (req, res, next) => {
     const {password} = req.query;
     if(password === 'chicken'){
         next();
     }
-    res.send('sorry, you need a password');
+    // res.send('please return, and with a password');
+    throw new Error('password required');
+};
+
+app.get('/error', (req, res) => {
+    chicken.fly();
 });
 
 app.get('/home', (req, res) => {
     res.render('home');
 });
 
-app.get('/secret', (req, res) => {
+app.get('/secret', verifyPassword, (req, res) => {
     res.send(req.arrivalDate);
 });
 
@@ -39,6 +45,19 @@ app.get('/dogs', (req, res) => {
 
 app.use((req, res) => res.send('NOT FOUND'));
 
+app.use((err, req, res, next) => {
+    console.log('##############ERROR#################');
+    console.log('##############ERROR#################');
+    console.log('##############ERROR#################');
+    next(err);
+    // res.status(500).send('yougot anerror');
+});
+
 app.listen(3000, () => {
     console.log('listening on 3000');
 });
+
+
+
+
+WHEN YOU SIT BACK DOWN, WE ARE GOING TO CONTINUE WITH ERROR HANDLING IN EXPRESS
